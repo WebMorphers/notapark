4a1e5ae6657e47d737522608ca8d02bd1b2b10a8
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-11 18:00:16 +0000

    9

--- notapark/src/components/hero/hero.tsx
+++ notapark/src/components/hero/hero.tsx
@@ -3,8 +3,8 @@ import im from "../../assets/image.png";
 const hero = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center top-0 relative pointer-events-none max-lg:mt-24 ">
-        <img src={im} alt="" className="h-[55%] w-[55%]" />
+      <div className="flex flex-col justify-center items-center top-0 relative pointer-events-none max-lg:mt-24   ">
+        <img src={im} alt="" className="h-[55%] w-[55%] " />
         <h1
           className="text-4xl  max-sm:text-xl lg:text-6xl drop-shadow-2xl "
           id="h1"
