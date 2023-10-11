cc8c465b57aade3239fae71bf95b6c31e53533f5
othsoh <sohabsouri@gmail.com>
2023-10-11 22:02:16 +0100

    t

--- notapark/src/components/hero/hero.tsx
+++ notapark/src/components/hero/hero.tsx
@@ -3,10 +3,10 @@ import im from "../../assets/image.png";
 const hero = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center top-0 relative pointer-events-none max-lg:mt-24   ">
-        <img src={im} alt="" className="h-[55%] w-[55%] " />
+      <div className="flex flex-col justify-center items-center  ">
+        <img src={im} alt="" className="h-auto w-[55%] object-cover max-sm:w-[100%] " />
         <h1
-          className="text-4xl  max-sm:text-xl lg:text-6xl drop-shadow-2xl "
+          className="text-4xl  max-sm:text-xl lg:text-6xl "
           id="h1"
         >
           Hoodies that skate with you
