9d11288ecd2ee7dc707d87a7df193273e9051d96
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-11 00:59:52 +0000

    Update hero.tsx

--- notapark/src/components/hero/hero.tsx
+++ notapark/src/components/hero/hero.tsx
@@ -1,15 +1,19 @@
- import "./hero.css"
- import im from '../../assets/image.png'
+import "./hero.css";
+import im from "../../assets/image.png";
 const hero = () => {
   return (
     <>
-    <div className='flex flex-col justify-center items-center  relative max-lg:mt-24 '> 
-    <img src={im} alt="" className='h-[55%] w-[55%]' />
-    <h1 className='text-4xl  max-sm:text-xl lg:text-6xl ' id='h1'>Hoodies that skate with you</h1>
-
-    </div>
+      <div className="flex flex-col justify-center items-center  relative max-lg:mt-24 ">
+        <img src={im} alt="" className="h-[55%] w-[55%]" />
+        <h1
+          className="text-4xl  max-sm:text-xl lg:text-6xl drop-shadow-2xl "
+          id="h1"
+        >
+          Hoodies that skate with you
+        </h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default hero
+export default hero;
