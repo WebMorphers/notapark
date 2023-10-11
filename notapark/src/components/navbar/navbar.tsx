f9941b933287af0a6b649d35a21133f67eed075b
othsoh <sohabsouri@gmail.com>
2023-10-11 16:32:42 +0100

    t

--- notapark/src/components/navbar/navbar.tsx
+++ notapark/src/components/navbar/navbar.tsx
@@ -1,5 +1,4 @@
 import logo from '../../assets/logo1.png'
-import './nav.css'
 
 import { useEffect, useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
@@ -40,7 +39,7 @@ const navbar = () => {
         <ul className="hidden md:flex">
           <li className="p-4">
             <a
-              className="no-underline mx-4 cursor-pointer font-normal text-gray-900 hover:text-red-500 qqsd "
+              className="no-underline mx-4 cursor-pointer font-normal text-gray-900 hover:text-red-500"
               onClick={() => scrollToSection("about_us")}
               href="#about_us"
             >
@@ -49,7 +48,7 @@ const navbar = () => {
           </li>
           <li className="p-4">
             <a
-              className="no-underline mx-4 cursor-pointer font-normal text-gray-900 hover:text-red-500 qqsd"
+              className="no-underline mx-4 cursor-pointer font-normal text-gray-900 hover:text-red-500"
               onClick={() => scrollToSection("projects")}
               href="#projects"
             >
@@ -59,7 +58,7 @@ const navbar = () => {
           <li className="p-4">
             {" "}
             <a
-              className="no-underline mx-4 cursor-pointer font-normal	text-gray-900 hover:text-red-500 qqsd"
+              className="no-underline mx-4 cursor-pointer font-normal	text-gray-900 hover:text-red-500"
               onClick={() => scrollToSection("contact")}
               href="#contact"
             >
@@ -78,7 +77,7 @@ const navbar = () => {
         <ul
           className={
             nav
-              ? " fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-amber-700 ease-in-out duration-500 "
+              ? " fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-amber-700 ease-in-out duration-500"
               : "ease-in-out duration-500 fixed left-[-100%]"
           }
         >
@@ -87,30 +86,30 @@ const navbar = () => {
           </a>
           <li className="p-4 ">
             <a
-              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 qqsd"
+              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600"
               onClick={() => scrollToSection("about_us")}
               href="#about_us"
             >
-              home
+              ABOUT US
             </a>
           </li>
           <li className="p-4">
             <a
-              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 qqsd "
+              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
               onClick={() => scrollToSection("projects")}
               href="#projects"
             >
-              About us
+              OUR PROJECTS
             </a>
           </li>
           <li className="p-4">
             {" "}
             <a
-              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 qqsd "
+              className="no-underline mx-4 cursor-pointer font-thin text-white hover:text-orange-600 "
               onClick={() => scrollToSection("contact")}
               href="#contact"
             >
-              Our products
+              CONTACT US
             </a>
           </li>
         </ul>
