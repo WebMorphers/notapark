8370ea41a72f9d2959d1fdc72efbd2a422efbc46
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-11 23:42:28 +0000

    Update about.tsx

--- notapark/src/components/about/about.tsx
+++ notapark/src/components/about/about.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import img1 from '../../assets/1.png';
+ import img1 from '../../assets/1.png';
 import img2 from '../../assets/2.png';
 import img3 from '../../assets/3.png';
 
 const About = () => {
   return (
-    <div className="bg-gray-200 p-4">
-      <h1 className="text-2xl mb-4">About Page</h1>
+    <div className="bg-gray-200 p-4   ">
+      <h1 className="text-2xl mb-4">Unique Design, Limited Edition</h1>
 
-      <div className="relative w-full h-96 overflow-hidden">
-        <img src={img1} alt="Image 1" className="absolute transition-transform transform hover:scale-125" />
-        <img src={img2} alt="Image 2" className="absolute transition-transform transform hover:scale-125 ml-40" />
-        <img src={img3} alt="Image 3" className="absolute transition-transform transform hover:scale-125 ml-96" />
+      <div className="flex justify-center items-center">
+        <a href="#1"><img src={img1} alt="Image 1" className="transition-transform transform hover:scale-125" /></a>
+        <a href="#2"><img src={img2} alt="Image 2" className="transition-transform transform hover:scale-125" /></a>
+        <a href="#2"><img src={img3} alt="Image 3" className="transition-transform transform hover:scale-125" /></a>
       </div>
     </div>
   );
