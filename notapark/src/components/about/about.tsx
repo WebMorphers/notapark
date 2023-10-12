2f79fa9b528c24aded70d993b76c3cfea704303f
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-12 00:36:45 +0000

    f

--- notapark/src/components/about/about.tsx
+++ notapark/src/components/about/about.tsx
@@ -4,11 +4,11 @@ import img3 from '../../assets/3.png';
 
 const About = () => {
   return (
-    <div className="bg-gray-200 p-4   ">
-      <h1 className="text-2xl mb-4">Unique Design, Limited Edition</h1>
+    <div className="bg-gray-200 p-4  flex justify-center items-center flex-col">
+      <h1 className="text-2xl mb-4 max-md:text-xl">Unique Design, Limited Edition , Superior Quality , Made In Morocco</h1>
 
-      <div className="flex justify-center items-center">
-        <a href="#1"><img src={img1} alt="Image 1" className="transition-transform transform hover:scale-125" /></a>
+      <div className="flex justify-center items-center  ">
+        <a href="#1"><img src={img1} alt="Image 1" className="transition-transform transform hover:scale-125 " /></a>
         <a href="#2"><img src={img2} alt="Image 2" className="transition-transform transform hover:scale-125" /></a>
         <a href="#2"><img src={img3} alt="Image 3" className="transition-transform transform hover:scale-125" /></a>
       </div>
