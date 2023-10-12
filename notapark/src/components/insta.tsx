3c69758c817950b0a8408907aea0efc37a3ff039
othsoh <sohabsouri@gmail.com>
2023-10-12 01:15:31 +0100

    Update insta.tsx

--- notapark/src/components/insta.tsx
+++ notapark/src/components/insta.tsx
@@ -5,7 +5,7 @@ const insta = () => {
         window.open('https://www.instagram.com/nota.park/', '_blank');
       };
   return (
-    <div className=" block"> 
+    <div className=" block lg:hidden"> 
         <button onClick={handleButtonClick}   className="fixed flex justify-center rounded-[50%] bg-inherited border-2 border-[black] bottom-20 right-10 w-10 h-10 items-center cursor-pointer transition-transform transform hover:scale-125 group bg-white ">
             <img src={ins} alt="Instagram" className="w-6 h-6  " />
          </button>
