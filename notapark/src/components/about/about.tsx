a4a21f9a68f8ba5382b66d75d571f64c639a6865
othsoh <sohabsouri@gmail.com>
2023-10-12 01:10:29 +0100

    f

--- notapark/src/components/about/about.tsx
+++ notapark/src/components/about/about.tsx
@@ -6,14 +6,32 @@ const About = () => {
   return (
     <div className="bg-gray-200 p-4">
       <h1 className="text-2xl mb-4">About Page</h1>
-      <div className='flex justify-center '>
-        <div className="relative w-full h-96 overflow-hidden left-0 right-0 transform -translate-1/2 ">
-          <img src={img1} alt="Image 1" className="absolute transition-transform transform hover:scale-125 max-sm:w-64" />
-          <img src={img2} alt="Image 2" className="absolute transition-transform transform hover:scale-125 ml-40  max-md:ml-32 max-sm:w-64 max-sm:ml-24 " />
-          <img src={img3} alt="Image 3" className="absolute transition-transform transform hover:scale-125 ml-96  max-md:ml-64 max-sm:w-64 max-sm:ml-48" />
-        </div>
-
-    </div>
+      <div className='flex items-center'>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 ">
+        <img
+          src={img1}
+          alt="Image 1"
+          className="w-full h-auto transform transition-transform hover:scale-125"
+        />
+        <img
+          src={img2}
+          alt="Image 2"
+          className="w-full h-auto transform transition-transform hover:scale-125"
+        />
+        <img
+          src={img3}
+          alt="Image 3"
+          className="w-full h-auto transform transition-transform hover:scale-125"
+        />
+      </div>
+      <div>
+        <h1 className='text-3xl'> Popular Products</h1>
+        <p className='text-sm'>Shop our collecton of graphic tees. All are screenprinted on
+ our classic 100% cotton t-shirts. Mix and match with a wide
+                variety of colors and graphics.
+Our collection is available in sizes S, M & L.</p><br/>
+      </div>
+      </div>
     </div>
   );
 };
