5f8ec260f571ada84006a0c9bccd946d2e505625
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-10-12 22:12:50 +0000

    f

--- notapark/src/App.tsx
+++ notapark/src/App.tsx
@@ -1,36 +1,28 @@
- 
-import './App.css'
-import Navbar from './components/navbar/navbar'
-import Hero from './components/hero/hero' 
-import Footer from './components/footer/footer'
-import Products from './components/products/products'
-import About from './components/about/about'
-import Deliv from './components/deliv/deliv'
-import Insta from './components/insta'
-import Abt from './components/abt/abt'
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Footer from "./components/footer/footer";
+import Products from "./components/products/products";
+import About from "./components/about/about";
+import Deliv from "./components/deliv/deliv";
+import Insta from "./components/insta";
+import Abt from "./components/abt/abt";
 
-
-
-
-
-function App() { 
-
+function App() {
   return (
-      <> 
-        <Navbar />
-        <Insta />
-        <div className='body flex flex-col gap-10 lg:gap-20'>
-          <Hero />
-          <Products />
-        </div>
-        <Deliv />
-        <Abt />
-        <About />
-        <Footer />
-        
-        
-      </>
-  )
+    <>
+      <Navbar />
+      <Insta />
+      <div className="body flex flex-col gap-10 lg:gap-20">
+        <Hero />
+        <Products />
+      </div>
+      <Deliv />
+      <Abt />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
